af75a80ca1e78c85acb8d2dca80a29aabe31972c
Felipe <felipemiguelfutebol@hotmail.com>
2026-06-20 13:25:30 -0300

    fix: Adiciona o @Column na minha classe

--- src/main/java/com/BatismoDeJava/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/com/BatismoDeJava/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -18,13 +18,20 @@ public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "id_ninja")
     private Long id;
 
+    @Column(name = "nome")
     private String nome;
+
+    @Column(name = "img_url")
+    private String imgUrl;
 
     @Column(unique = true)
     private String email;
 
+    @Column(name = "idade")
     private int idade;
 
     //Muitos ninjas podem ter a mesma missao
--- src/main/java/com/BatismoDeJava/CadastroDeNinjas/Ninjas/NinjaRepository.java
+++ src/main/java/com/BatismoDeJava/CadastroDeNinjas/Ninjas/NinjaRepository.java
@@ -1,5 +1,10 @@
 package com.BatismoDeJava.CadastroDeNinjas.Ninjas;
 
-public class NinjaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//ISSO DAQUI É PARA INICIAR O MEU BANCO DE DADOS
+//Extends o JPA repository, coloca a classe e o tipo da chave da classe
+//"ORM" SERVE PARA TRADUZIR O QUE ESCREVEMOS PARA O BANCO DE DADOS
+public interface NinjaRepository extends JpaRepository<NinjaModel, Long>{
 
 }
