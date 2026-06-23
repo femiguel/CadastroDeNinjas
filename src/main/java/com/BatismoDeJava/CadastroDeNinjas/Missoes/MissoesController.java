524eb8db7d55347ffe822cead4ea389018567100
Felipe <felipemiguelfutebol@hotmail.com>
2026-06-22 21:35:26 -0300

    chore: .gitgnore adiciona as minhas credenciais do BD

--- src/main/java/com/BatismoDeJava/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/com/BatismoDeJava/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,7 +1,11 @@
 package com.BatismoDeJava.CadastroDeNinjas.Missoes;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/missoes")
 public class MissoesController {
     @GetMapping("/missoes")
     public String missoes() {
