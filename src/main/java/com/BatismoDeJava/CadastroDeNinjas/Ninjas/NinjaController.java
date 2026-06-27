66cce02d8d8c36da2aa9c23284341e1726715c5e
Felipe <felipemiguelfutebol@hotmail.com>
2026-06-27 13:16:54 -0300

    feat: cria endpoints iniciais do CRUD de ninja

--- src/main/java/com/BatismoDeJava/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/com/BatismoDeJava/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package com.BatismoDeJava.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/ninja")
@@ -14,5 +12,34 @@ public class NinjaController {
         return "Essa é a minha primeira mensagem nessa rota";
     }
 
+    //ADICIONAR NINJA (CREATE)
+    @PostMapping("/criar")
+    public String criarNinja(){
+        return "Ninja criado";
+    }
 
+    //MOSTRAR NINJA POR ID (READ)
+    @GetMapping("/todosId")
+    public String mostrarNinjaPorId(){
+        return "Mostrar ninjas por iD";
+    }
+
+    //MOSTRAR OS NINJAS (READ)
+    @GetMapping("/todos")
+    public String mostrarTodosOsNinjas(){
+        return "Mostrar ninjas";
+    }
+
+    //ALTERAR DADOS DOS NINJAS (UPDATE)
+    @PutMapping("/alterar")
+    public String alterarNinjaPorId(){
+    return "Alterar ninja por Id";
+    }
+
+
+    //DELETAR NINJA (DELETE)
+    @DeleteMapping("/deletarID")
+    public String deletarNinjaPorID(){
+        return "O ninja deletado por ID";
+    }
 }
