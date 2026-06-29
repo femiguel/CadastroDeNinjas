5475b5123de6c417f2417b95649b4419ebb081ae
Felipe <felipemiguelfutebol@hotmail.com>
2026-06-29 10:14:42 -0300

    feat: cria as rotas do controller com mock

--- src/main/java/com/BatismoDeJava/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/com/BatismoDeJava/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,14 +1,39 @@
 package com.BatismoDeJava.CadastroDeNinjas.Missoes;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+//CONTROLAR A ROTA
 @RestController
+//CRIAR O MEU CAMINHO INICIAL
 @RequestMapping("/missoes")
 public class MissoesController {
     @GetMapping("/missoes")
     public String missoes() {
         return "Essa são as minhas missoes Ninjas";
+
+    }
+
+    //CRIAR
+    @PostMapping("/criar")
+    public String criarMissoes() {
+        return "Criar missão";
+    }
+
+    //READ
+    @GetMapping("/ler")
+    public String ler(){
+        return "Listar missoes";
+    }
+
+    //UPDATE
+    @PutMapping("/update")
+    public String update(){
+        return "Atualizar missao";
+    }
+
+    //DELETE
+    @DeleteMapping("/deletar")
+    public String delete(){
+        return "Deletar missao";
     }
 }
