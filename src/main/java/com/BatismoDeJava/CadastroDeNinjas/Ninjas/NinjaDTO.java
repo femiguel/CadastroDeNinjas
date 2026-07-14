28b2cfa1200003626d44bd3631182c6ece5fb9ea
Felipe <felipemiguelfutebol@hotmail.com>
2026-07-13 21:10:20 -0300

    chore: inclui a versao do DTO pela entidade e mapeia manualmente no ninjaMapper os caminhos entidade x dto

--- src/main/java/com/BatismoDeJava/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/com/BatismoDeJava/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,22 @@
 package com.BatismoDeJava.CadastroDeNinjas.Ninjas;
 
+import com.BatismoDeJava.CadastroDeNinjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private String imgUrl;
+    private String email;
+    private int idade;
+    private String rank;
+    private MissoesModel missoes;
+
+
 }
