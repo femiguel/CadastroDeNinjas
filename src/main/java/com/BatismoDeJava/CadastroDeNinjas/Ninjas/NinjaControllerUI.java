16a3ac94a49a76bf8d98cb6269020f68c40594dc
Felipe <felipemiguelfutebol@hotmail.com>
2026-07-22 08:18:19 -0300

    feat: adiciona página HTML da aplicação

---
+++ src/main/java/com/BatismoDeJava/CadastroDeNinjas/Ninjas/NinjaControllerUI.java
@@ -0,0 +1,29 @@
+package com.BatismoDeJava.CadastroDeNinjas.Ninjas;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/ninja/ui")
+public class NinjaControllerUI {
+
+    private final NinjaService ninjaService;
+
+    public NinjaControllerUI(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
+
+    @GetMapping("/ler")
+    public String listarNinjas(Model model) {
+        List<NinjaDTO> ninjas = ninjaService.listarNinja();
+        model.addAttribute("ninjas", ninjas);
+        return "listarNinjas";
+    }
+
+
+}
